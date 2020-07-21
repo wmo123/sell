735fafbe4746816171f0692ddbe7739f265ebaa9
王敏 <wangmint@yonyou.com>
2020-07-21 23:17:33 +0800

    add: 商品展示

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,7 +3,7 @@
     <div class="menu-wrapper">
       <ul>
         <li class="menu-item" v-for="(item, index) in goods" :key="index">
-          <div class="table-cell border-1px" >
+          <div class="table-cell border-1px">
             <span
               v-show="item.type > 0"
               class="icon"
@@ -14,7 +14,34 @@
         </li>
       </ul>
     </div>
-    <div class="goods-wrapper"></div>
+    <div class="goods-wrapper">
+      <div class="good-item" v-for="(good, index) in goods" :key="index">
+        <h2 class="title">{{ good.name }}</h2>
+        <div
+          class="item border-1px"
+          v-for="(food, index) in good.foods"
+          :key="index"
+        >
+          <div class="image">
+            <img width="64px" height="64px" :src="food.image" />
+          </div>
+          <div class="content">
+            <div class="food-name">{{ food.name }}</div>
+            <div class="food-desc">{{ food.description }}</div>
+            <div class="sell">
+              <span class="count">月售{{ food.sellCount }}份</span
+              ><span class="rating">好评率{{ food.rating }}%</span>
+            </div>
+            <div class="good-price">
+              <span class="price"><i class="symbol">¥</i>{{ food.price }}</span>
+              <span v-if="food.oldPrice" class="old-price"
+                ><i class="symbol">¥</i>{{ food.oldPrice }}</span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -94,4 +121,51 @@ export default {
           line-height 14px
   .goods-wrapper
     flex 1
+    .good-item
+      .title
+        font-size 12px
+        line-height 26px
+        color rgb(147,153,159)
+        padding-left 14px
+        background #f3f5f7
+        height 26px
+        border-left 2px solid #d9dde1
+      .item
+        display flex
+        margin 18px 18px 0
+        border-1px(rgba(7,17,27,0.1))
+
+        .image
+          flex 0 0 64px
+          margin-right 10px
+        .content
+          flex 1
+          overflow hidden
+          .food-name
+            margin-top 2px
+            font-size 14px
+            line-height 14px
+            color rgb(7,17,27)
+          .food-desc,.sell
+            font-size 10px
+            line-height 10px
+            color rgb(147,153,159)
+            margin 8px 0
+          .sell
+            margin-top 0
+            .count
+              margin-right 12px
+          .good-price
+            .price
+              font-size 14px
+              color rgb(240,20,20)
+              font-weight 700
+              line-height 24px
+              margin-right 8px
+              .symbol
+                font-size 10px
+                color rgb(240,20,20)
+                font-weight normal
+                font-style normal
+                margin-right 3px
 </style>
